3323981cdc9927d5f3e27bb6b3563e44c091602c
Asel Esra Ozyilmaz <esra.ozyilmaz@iaau.edu.kg>
2022-12-13 17:36:25 +0600

    added alatoo's logo

--- src/components/Image.js
+++ src/components/Image.js
@@ -6,13 +6,14 @@ import image2 from './asset/tr2.jpg';
 import image3 from './asset/tr3.jpg';
 import image4 from './asset/tr4.jpg';
 import image5 from './asset/tr5.jpg';
+import image6 from './asset/alatoo logo.jpg';
 import '../App.css';
 
 
 const Image = () => {
   const [selectedImage, setSelectedImage] = useState(0)
   const [allImages, setAllImages] = useState([image,image0, image1, image2, image3, image4, image5])
-  
+
   useEffect(() => {
     setInterval(() => {
       setSelectedImage(selectedImage => selectedImage > 3 ? 0 : selectedImage + 1)
@@ -32,6 +33,7 @@ const Image = () => {
       }}
        className='btn'>Click to travel around TURKIYE !</button>
       </div>
+      <img src={image6} alt="logo" width={60} className="logo"/>
     </div>
   )
 }
